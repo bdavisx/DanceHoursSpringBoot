b37c04bf87a89d7ee585cd633363d72efb960f81
Bill Davis <bdavisx@yahoo.com>
2016-01-23 12:35:20 -0600

    Added flyway integration.

---
+++ src/main/resources/db/migration/v2__First_User_Tables.sql
@@ -0,0 +1,25 @@
+create table dance_user (
+    user_id   uuid primary key not null,
+    email     varchar(254)     not null,
+    full_name varchar(1024)    not null,
+    is_active bool             not null
+);
+
+create table dance_user_roles (
+    user_id uuid not null,
+    role_id uuid not null,
+    primary key (user_id, role_id)
+);
+
+create table user_roles (
+    role_id     uuid primary key not null,
+    name        varchar(255),
+    description text,
+    is_admin    bool
+);
+
+create table aggregate_passwords (
+    aggregate_id  uuid primary key not null,
+    password_hash bytea            not null,
+    salt          bytea            not null
+);
