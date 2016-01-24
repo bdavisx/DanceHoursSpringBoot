bba781dc05a759e137cc11994430104b0f55c2fd
Bill Davis <bdavisx@yahoo.com>
2016-01-24 10:11:16 -0600

    Probably aren't going to need this for this app now.

--- src/main/java/com/tartner/dancehours/domain/repositories/HasSnapshot.kt
+++
@@ -1,6 +0,0 @@
-package com.tartner.dancehours.domain.repositories
-
-/** Points to a class that has snapshots. */
-interface HasSnapshot {
-    public var Snapshot: Snapshot
-}
--- src/main/java/com/tartner/dancehours/domain/repositories/Snapshot.kt
+++
@@ -1,4 +0,0 @@
-package com.tartner.dancehours.domain.repositories
-
-/** Marker interface for snapshots. */
-interface Snapshot {}
