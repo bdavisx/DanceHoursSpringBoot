5e27890d1c9ab0ab02e6bae62c2723a77c278d68
Bill Davis <bdavisx@yahoo.com>
2016-03-07 18:06:36 -0600

    First component working like I want it to (not really doing anything).

--- src/main/java/com/tartner/dancehours/service/DanceUserDetails.kt
+++ src/main/java/com/tartner/dancehours/service/DanceUserDetails.kt
@@ -1,12 +1,13 @@
 package com.tartner.dancehours.service
 
+import com.tartner.dancehours.DanceHoursId
 import com.tartner.dancehours.domain.danceuser.DanceUserAggregate
 import com.tartner.utilities.Empty
 import com.tartner.utilities.emptyUUID
 import java.util.*
 
 class DanceUserDetails() {
-    public var id: UUID = emptyUUID()
+    public var id: DanceHoursId = DanceHoursId.Empty
     public var fullName: String = String.Empty
     public var email: String = String.Empty
     public var isActive: Boolean = true
@@ -14,10 +15,10 @@ class DanceUserDetails() {
     companion object {
         public fun from(user: DanceUserAggregate): DanceUserDetails {
             val userDetails = DanceUserDetails();
-            userDetails.id = user.id;
-            userDetails.fullName = user.fullName
-            userDetails.email = user.email
-            userDetails.isActive = user.isActive
+//            userDetails.id = user.id;
+//            userDetails.fullName = user.fullName
+//            userDetails.email = user.email
+//            userDetails.isActive = user.isActive
             return userDetails;
         }
     }
